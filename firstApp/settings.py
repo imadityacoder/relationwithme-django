cd55ce804c9a0ac6998e4a262add74c2369bb3dd
imadityacoder <iamadityacoder01@gmail.com>
2023-12-16 14:14:19 +0530

    fix vercel settings

--- firstApp/settings.py
+++ firstApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)8pwbf15353rz+ocj_bln8p102eds(!th$256#@e#%=9k%=e67
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','.now.sh']
 
 
 # Application definition
@@ -118,7 +118,8 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
